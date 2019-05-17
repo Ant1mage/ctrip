fd24099baf292d310adce59948f820f9ac4dd6c7
Alexander Zou <sugaralex@163.com>
2019-05-18 00:43:03 +0800

    Initial commit

---
+++ lib/widget/grid_nav_widget.dart
@@ -0,0 +1,16 @@
+import 'package:ctrip/model/gridnav_model.dart';
+import 'package:flutter/material.dart';
+
+class GridNavWidget extends StatelessWidget {
+
+  final GridNavModel gridNavModel;
+
+  const GridNavWidget({Key key, @required this.gridNavModel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
